54969392346d5c347ad1db9c6849adb63b515ecc
Riley Moore <rilemoore@gmail.com>
2019-11-18 21:23:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do 
-    "Welcome to your app!!!! I BUILT THIS! "
+    "Hello, World!"
   end
 
 end
